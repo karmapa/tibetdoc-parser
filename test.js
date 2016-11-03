24eef96b9637c94d64e040c170cbf0555eec96e2
rickie120243 <rickie120243@gmail.com>
2016-11-03 18:16:24 +0800

    add check

--- test.js
+++ test.js
@@ -3,13 +3,19 @@ var parser = require('./src/parser.js');
 var content = fs.readFileSync('./test.dct', 'binary');
 var assert = require('assert');
 
-describe('convert', function () {
-  it('convert check', function () {
+describe('convert ', function () {
+  var staging;
+  var correct = '༄༅། །གྲུབ་ཐོབ་བརྒྱད་ཅུ་རྩ་བཞིའི་ལོ་རྒྱུས་སློབ་དཔོན་མི་འཇིགས་པ་སྦྱིན་པ་དཔལ་གྱིས་མཛད་པ་བཞུགས་སོ། །';
+  it('convert check with html tag', function () {
     var data = parser.parse(content.toString());
     data = parser.JSONToHTML(data);
-    var correct = '༄༅། །གྲུབ་ཐོབ་བརྒྱད་ཅུ་རྩ་བཞིའི་ལོ་རྒྱུས་སློབ་དཔོན་མི་འཇིགས་པ་སྦྱིན་པ་དཔལ་གྱིས་མཛད་པ་བཞུགས་སོ། །';
+    staging = data;
     var result = data.match(correct);
-    console.log(result);
+    assert.equal(result, correct);
+  });
+  it('remove html tag', function () {
+    var result = parser.HTMLtoText(staging).replace(/\n/g, '');
+    staging = result;
     assert.equal(result, correct);
   });
 });
